7db0bab5c99fa5582d1776eb04573ddd348db2d6
Ayman Adlan <aymanadlan@hotmail.com>
2024-08-05 19:51:13 +0400

    Solving race conditions with optimistic concurrency

--- Bookify.Application/Bookings/ReserveBooking/ReserveBookingCommandHandler.cs
+++ Bookify.Application/Bookings/ReserveBooking/ReserveBookingCommandHandler.cs
@@ -1,6 +1,7 @@
 ﻿
 using Bookify.Application.Abstractions.Clock;
 using Bookify.Application.Abstractions.Messaging;
+using Bookify.Application.Exceptions;
 using Bookify.Domain.Abstractions;
 using Bookify.Domain.Apartments;
 using Bookify.Domain.Bookings;
@@ -18,10 +19,10 @@ internal sealed class ReserveBookingCommandHandler : ICommandHandler<ReserveBook
     private readonly PricingService _pricingService;
     private readonly IDateTimeProvider _dateTimeProvider;
 
-    public ReserveBookingCommandHandler(IUserRepository userRepository, 
-        IApartmentRepository apartmentRepository, 
-        IBookingRepository bookingRepository, 
-        IUnitOfWork unitOfWork, 
+    public ReserveBookingCommandHandler(IUserRepository userRepository,
+        IApartmentRepository apartmentRepository,
+        IBookingRepository bookingRepository,
+        IUnitOfWork unitOfWork,
         PricingService pricingService,
         IDateTimeProvider dateTimeProvider)
     {
@@ -49,24 +50,31 @@ internal sealed class ReserveBookingCommandHandler : ICommandHandler<ReserveBook
             return Result.Failure<Guid>(ApartmentErrors.NotFound);
         }
 
-        var duration = DateRange.Create(request.StartDate,request.EndDate);
+        var duration = DateRange.Create(request.StartDate, request.EndDate);
 
-        if (await _bookingRepository.IsOverlappingAsync(apartment,duration,cancellationToken))
+        if (await _bookingRepository.IsOverlappingAsync(apartment, duration, cancellationToken))
         {
             return Result.Failure<Guid>(BookingErrors.Overlap);
         }
 
-        var booking = Booking.Reserve(
-            apartment,
-            user.Id,
-            duration,
-            _dateTimeProvider.UtcNow,
-            _pricingService);
+        try
+        {
+            var booking = Booking.Reserve(
+                apartment,
+                user.Id,
+                duration,
+                _dateTimeProvider.UtcNow,
+                _pricingService);
 
-        _bookingRepository.Add(booking);
+            _bookingRepository.Add(booking);
 
-        await _unitOfWork.SaveChangesAsync(cancellationToken);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
 
-        return booking.Id;
+            return booking.Id;
+        }
+        catch (ConcurrencyException)
+        {
+            return Result.Failure<Guid>(BookingErrors.Overlap);
+        }
     }
 }
